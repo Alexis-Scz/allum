6e12b93a5bca03755fad5a0f856c2af7a39d95db
william <45491256+williamloree@users.noreply.github.com>
2024-09-23 20:14:29 +0200

    Update allum.py

--- allum.py
+++ allum.py
@@ -1,31 +1,28 @@
 import time
+
+nb_allumette = 20
+joueur = 1
+seconde = 5
+
 print("Début de la partie : ")
-nbalum=10
-print(" I "* nbalum)
-joueur=1
-while nbalum>0:
-    tej=int(input("Combien d'allum tu veux retirer le sang ? (entre 1 et 3) "))
-    if tej<=nbalum:
-        if 1<=tej<=3:
-            nbalum=nbalum-tej
-            print(" I " * nbalum)
-            print("")
-            if joueur==1:
-                joueur=2
-            else:
-                joueur=1
-            print("Tour du joueur N°",str(joueur))
-            
+print(" I " * nb_allumette)
+
+while nb_allumette > 0:
+    allumette_retire = int(input("Combien d'allum tu veux retirer le sang ? (entre 1 et 3)"))
+    if allumette_retire <= nb_allumette:
+        if 1 <= allumette_retire <= 3 :
+            nb_allumette = nb_allumette - allumette_retire
+            print(" I " * nb_allumette)
+            joueur += 1
+            print("Tour du joueur N°", str(joueur % 2 + 1))
         else:
-            print("azy abuse pas je t'ai dit entre 1 et 3 là ")
+            print("azy abuse pas je t'ai dit entre 1 et 3 là")
     else:
-        print("Abuse pas frérot retire pas trop ")
+        print("Abuse pas frérot retire pas trop")
 
+print("Joueur", str(joueur % 2 + 1), "gagne ")
 
-print("Joueur",str(joueur), "gagne ")
-   
-a=5
-while a>0:
-        a=a-1    
-        print("Fermeture dans : ", str(a))
-        time.sleep(1)                   
+while seconde > 0:
+        seconde = seconde - 1    
+        print("Fermeture dans : ", str(seconde))
+        time.sleep(1)      
